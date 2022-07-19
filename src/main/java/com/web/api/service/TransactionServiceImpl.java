006c0e388967773310b1171010d2597029eb5e2e
Fausto <Fausto.Espinoza.-ND@disney.com>
2022-07-19 12:51:59 -0400

    deteted code unnecesary.

--- src/main/java/com/web/api/service/TransactionServiceImpl.java
+++ src/main/java/com/web/api/service/TransactionServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
